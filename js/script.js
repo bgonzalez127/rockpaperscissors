0d386871a3632278658b7c770befff74e3228ab1
Brianna Gonzalez <bgonzalez2019@brooklynprephs.org>
2018-12-05 22:44:58 +0000

    addedjquery

--- js/script.js
+++ js/script.js
@@ -11,6 +11,50 @@
 
 $(function () {
     $("#shoot").click(function(){
-        $("#userChoice").text( $("#input").val());
+        var compchoice = "";
+        var x = Math.random();
+        if(x <= .33) {
+            compchoice = "Rock";
+        } else if (x <= .66) {
+            compchoice = "Paper";
+        } else {
+          compchoice = "Scissor";  
+        }
+        
+        var userchoice =  $("#input").val();
+        
+        // we will assign these based on the result below
+        var tie = false;
+        var userWon = false;
+        
+        if(userchoice === compchoice){
+            tie = true;
+        } else if (userchoice === "Scissor" && compchoice === "Paper"){
+            userWon = true;
+        }else if (userchoice === "Rock" && compchoice === "Scissor"){
+            userWon = true;
+        }else if (userchoice === "Paper" && compchoice === "Rock"){
+            userWon = true;
+        }
+        
+        var result = "";
+        // tie, you lost, you won
+        if(tie) {
+            result = "You tied yo";
+        } else if(userWon) {
+            result = "You won yo";
+        } else {
+            result = "You lost, dumby";
+        }
+         
+        
+        
+        // is tie true?
+        
+        console.log("hello", compchoice);
+        $("#userChoice").text(userchoice);
+        $("#computerChoice").text(compchoice);
+        $("#result").text(result);
+
     });
 });
